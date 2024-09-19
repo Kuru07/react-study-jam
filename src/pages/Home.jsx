18f862c1ffd44b68f6bce96e4ed9cb04b8c44d9f
Kuru07 <kumarakuru7007@gmail.com>
2024-09-19 23:25:55 +0530

    NavBar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <div className="bg-[#FFEBEE] text-[#B71C1C] pt-56 ">
+      Home Page
+    </div>
+    </>
+  )
+}
+
+export default Home
